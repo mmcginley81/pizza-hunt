b37c3ae82583867f6cd2d9b5a323658c3c40d6a8
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-04-12 15:57:15 -0700

    added get post delete routes for pizzas

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+// Set up GET all and POST at /api/pizzas
+
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+// Set up Get one, Put, and Delete at /api/pizzas/:id
+
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+module.exports = router
