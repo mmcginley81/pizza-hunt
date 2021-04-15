fe0ea4e4b2dcc85055b8089ab60585f4d2f6186f
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-04-14 18:27:49 -0700

    finished replies

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -17,8 +17,9 @@ function getPizza() {
   // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
     .then(response => {
-      // check for a 4xx or 5xx error from server
+      console.log(response);
       if (!response.ok) {
+        console.log('hi');
         throw new Error({ message: 'Something went wrong!' });
       }
 
@@ -29,7 +30,7 @@ function getPizza() {
       console.log(err);
       alert('Cannot find a pizza with this id! Taking you back.');
       window.history.back();
-    })
+    });
 }
 
 function printPizza(pizzaData) {
@@ -63,12 +64,14 @@ function printComment(comment) {
       <h5 class="text-dark">${comment.writtenBy} commented on ${comment.createdAt}:</h5>
       <p>${comment.commentBody}</p>
       <div class="bg-dark ml-3 p-2 rounded" >
-        ${comment.replies && comment.replies.length
-      ? `<h5>${comment.replies.length} ${comment.replies.length === 1 ? 'Reply' : 'Replies'
-      }</h5>
+        ${
+          comment.replies && comment.replies.length
+            ? `<h5>${comment.replies.length} ${
+                comment.replies.length === 1 ? 'Reply' : 'Replies'
+              }</h5>
         ${comment.replies.map(printReply).join('')}`
-      : '<h5 class="p-1">No replies yet!</h5>'
-    }
+            : '<h5 class="p-1">No replies yet!</h5>'
+        }
       </div>
       <form class="reply-form mt-3" data-commentid='${comment._id}'>
         <div class="mb-3">
@@ -125,7 +128,7 @@ function handleNewCommentSubmit(event) {
     })
     .then(commentResponse => {
       console.log(commentResponse);
-      location.reload();
+      // location.reload();
     })
     .catch(err => {
       console.log(err);
@@ -149,9 +152,31 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
-$backBtn.addEventListener('click', function () {
+$backBtn.addEventListener('click', function() {
   window.history.back();
 });
 
